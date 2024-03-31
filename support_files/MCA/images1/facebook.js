e87e437a5d665978d05b183e4ba4790e76870708
Saksham Aggarwal <saksham.aggarwal08@gmail.com>
2024-03-31 16:04:27 +0530

    removed v1 files

--- support_files/MCA/images1/facebook.js
+++
@@ -1,5 +0,0 @@
-
-document.write("<tr><td align=right><a href=http://www.facebook.com/sanmacs94><img src=http://www.sanmacs.com/MCA/mca_images/mca_f.gif alt=Facebook title=Facebook border=0></img></a></td>");
-document.write("<td align=left><a class=ft1 href=http://www.facebook.com/sanmacs94>Like Us</a></td></tr>");
-//document.write("<tr><td align=right><a href=http://twitter.com/#!/login><img src=http://www.sanmacs.com/MCA/mca_images/mca_t.gif alt=Twitter title=Twitter border=0></a></img></td>");
-//document.write("<td align=left><a class=ft1 href=http://twitter.com/#!/login>Follow Us</a></td></tr>");
